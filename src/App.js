8b60a8ec61d955fb4656bda77282f4c16ba89ac5
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2026-03-18 13:29:03 -0500

    remove twitter panel

--- src/App.js
+++ src/App.js
@@ -426,25 +426,7 @@ function XTimeline() {
       </div>
       </Col>
     </Row>
-    <Row xs={1} md={1}>
-      <Col className='shadow-sm mt-5 p-4 pt-5' id='twitter-m'>
-        <div className='container'>
-          <h2><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='icons'> <path fill='#00acee' d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg> TELL Recent Tweets</h2>
-          <hr/>
-          <Row id='twitter-list-m' className='shadow rounded'>
-            {/** 
-            <TwitterTimelineEmbed className='twitter-body-m'
-              sourceType="profile"
-              screenName="MizzouTell"
-              options={{
-                height: 500,
-                width: '100%'
-              }}
-            /> */}
-        </Row>
-        </div>
-      </Col>
-    </Row>
+
     <Row xs={1} md={1}>
     <Col className='shadow-sm mt-5 p-4 pt-5' id='home-row-m2'>
       <div className='container'>
@@ -610,25 +592,7 @@ function XTimeline() {
       <p><img src={require('./img/logo.png')} alt='logo'/>Learning Games for STEM Subjects and Computational Thinking</p>
       <p><img src={require('./img/logo.png')} alt='logo'/>Novel Technologies for Instructions and Learning</p>
       </Col>
-      <Col>
-      <div className='container row mt-3 pt-4 pd-4 shadow rounded' id='twitter-panel'>
-        <div className='col-md-1' id='twitter-logo-col'>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='icons' id='twitter-panel-logo'> <path fill='#00acee' d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
-        </div>
-        <div className='col-md-10 ms-3 ps-3 pe-3' id='twitter-scroll'>
-          <Row id='twitter-list' className='shadow rounded'>
-            <div className='col-md-9'>
-            <XTimeline/>
 
-            </div>
-            <div className='col-md-2'>
-              <img id='twitter-panel-mu-logo' src={require('./img/logo-reverse.png')} alt='logo' style={{position:'absolute', bottom:'5%', right:'10%'}} />
-            </div>
-        </Row>
-        </div>
-          
-        </div>
-      </Col>
       </Row>
       </Col>
 
